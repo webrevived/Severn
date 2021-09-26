74edf585559df17c5357c1108173b11ff87c0f02
Nathan <39814328+Yofou@users.noreply.github.com>
2021-09-26 04:16:25 +0100

    [📦] About page (header & about section)

--- frontend/src/routes/about.svelte
+++ frontend/src/routes/about.svelte
@@ -1,7 +1,14 @@
 <script>
     import NavBar from '$lib/global/Navigation/Bar.svelte'
+    import Header from '$lib/about/Header.svelte';
+    import About from '$lib/about/About.svelte';
 </script>
 
-<section class="x-container pt-6">
-    <NavBar dark />
-</section>
+<main>
+    <section class="x-container pt-6">
+        <NavBar dark />
+    </section>
+
+    <Header />
+    <About />
+</main>
