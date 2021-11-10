12afe2a7204ccc4e0614325ae2afbd55fdd2c9c8
Nathan <39814328+Yofou@users.noreply.github.com>
2021-11-10 14:10:45 +0000

    backend mysql config & api assets

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,8 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'admin'),
+        password: env('DATABASE_PASSWORD', 'Webrevived99!'),
       },
       options: {
         useNullAsDefault: true,
