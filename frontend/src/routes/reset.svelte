4c79175dbd78eee98d41c664b54ca9a141d33ae1
Nathan <39814328+Yofou@users.noreply.github.com>
2021-11-14 12:38:46 +0000

    [🔧] Fixed remaining pages

--- frontend/src/routes/reset.svelte
+++ frontend/src/routes/reset.svelte
@@ -1,36 +1,55 @@
-<script>
-	import Button from '$lib/global/Button.svelte';
-	import Checkbox from '$lib/global/Form/Checkbox.svelte';
-	import Input from '$lib/global/Form/Input.svelte';
-	import Header from '$lib/global/HeaderHR.svelte';
-	import IconHr from '$lib/global/IconHR.svelte';
-	import Bar from '$lib/global/Navigation/Bar.svelte';
-	import { onMount, stop_propagation } from 'svelte/internal';
+<script lang="ts" context="module">
+    import type { Load } from '@sveltejs/kit';
+    import { findAll } from "$lib/api/products"
+    import type { ProductsApi } from "$lib/api/products"
+
+    export const load: Load = async () => {
+        const products = await findAll()
+
+        return {
+            props: {
+                products
+            }
+        }
+    }
 </script>
 
+<script lang="ts">
+	import Button from '$lib/global/Button.svelte';
+	import Input from '$lib/global/Form/Input.svelte';
+	import Header from '$lib/global/HeaderHR.svelte';
+	import Bar from '$lib/global/Navigation/Bar.svelte';
 
-<section class="x-container pt-6">
-	<Bar dark />
-</section>
+	export let products: ProductsApi[]
+</script>
 
-<Header title="Reset Password" align="center">
-	Come join us for fast check and track your orders.
-</Header>
+<header class="x-container pt-6">
+	<Bar {products} dark />
+</header>
 
-<div class="border-black-600 border-1 rounded-lg max-w-2xl mx-auto p-8 mb-16">
-	<h2 class="text-black-600 font-semibold mb-8 text-xl">Forget password?</h2>
+<main>
+	<Header title="Reset Password" align="center">
+		Come join us for fast check and track your orders.
+	</Header>
+	
+	<section class="x-container">
+		<div class="border-black-600 border-1 rounded-lg max-w-full md:max-w-75/100 mx-auto p-8 mb-16">
+			<h2 class="heading-2 text-black-600 mb-8 ">Forget password?</h2>
+		
+			<div class="inputs flex flex-col gap-8">
+				<Input value="" placeholder="Email Address" />
+			</div>
+		
+			<div flex="~ col md:row" class="items-center justify-between my-8 gap-y-4">
+				<a href="/login" class="text-base font-semibold border-b-1">Cancel</a>
+				<Button>Submit</Button>
+			</div>
+		
+			<h3 class="text-base font-semibold  mx-auto block max-w-max border-b-1">
+				<a href="/contact">Contact customer support</a>
+			</h3>
+		</div>
+	</section>
+</main>
 
-	<div class="inputs flex flex-col gap-8">
-		<Input value="" placeholder="Email Address" />
-	</div>
-
-	<div class="flex items-center justify-between my-8">
-		<span class="text-base font-semibold border-b-1">Forgot your password</span>
-		<Button>Submit</Button>
-	</div>
-
-	<h3 class="text-base font-semibold  mx-auto block max-w-max border-b-1">
-		<a href="/contact">Contact customer support</a>
-	</h3>
-</div>
 
